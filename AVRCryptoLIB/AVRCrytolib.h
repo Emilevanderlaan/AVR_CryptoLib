fbe7665f5cb956e22a67dbec99573b13696df25f
Emile van der Laan <Github@emsign.nl>
2017-04-27 08:41:50 +0200

    Add files via upload
    Inital version in GitHub 
    Revision 0.6 16 Jan 2016
    Made it compatible for Avr Studio 7.0

---
+++ AVRCryptoLIB/AVRCrytolib.h
@@ -0,0 +1,21 @@
+#ifndef __AVRCrytolib_h__
+#define __AVRCrytolib_h__
+
+#include <avr/pgmspace.h>
+#include <string.h>
+
+#ifndef UPGM_P
+#define UPGM_P const unsigned char *
+#endif
+
+
+
+#include "des.h"
+#include "rsa.h"
+#include "sha.h"
+#include "md5.h"
+#include "SkipJack.h"
+#include "AES.h"
+
+#endif // __AVRCrytolib_h__
+
---
+++ AVRCrypto_Test/Debug/makedep.mk
@@ -0,0 +1,18 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+AES_test.c
+
+des_test.c
+
+main.c
+
+md5_test.c
+
+rsa_test.c
+
+sha_test.c
+
+SkipJack_test.c
+
